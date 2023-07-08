72d66a92895b25449ad0f4346b2df6b80d26d546
suresh <thesureshvadde@gmail.com>
2023-07-08 20:45:08 +0530

    data block

---
+++ 03-data/datasource.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+output "data" {
+  value = data.aws_ami.ami.image_id
+}
